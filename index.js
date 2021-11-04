21b6223d6833ee55a60650169075ab94df18192e
Tim Kravel <TKravel@Yahoo.com>
2021-11-04 17:17:45 -0400

    set up mongoDB connection and dotenv file/vars

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -5,18 +6,32 @@ const port = 3001;
 
 const app = express();
 
+// Connect to DB
+const mongoose = require('mongoose');
+mongoose.connect(
+	`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.ffprj.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
+	{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function () {
+	console.log('Connected to quitCalcDB');
+});
+
 const corsOptions = {
 	origin: true,
 	'Access-Control-Allow-Credentials': true,
 	credentials: true,
 };
 
+app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
 app.post('/test', (req, res) => {
-	res.header('Access-Control-Allow-Headers', '*');
-	res.header('Access-Control-Allow-Credentials', true);
+	const data = req.body;
+	console.log(data.username, data.password);
 	if (req.cookies) {
 		console.log('cookie detected');
 	}
