1e5967c21fa3ee734faa4f408b2cef79920707a2
Tim Kravel <TKravel@Yahoo.com>
2021-11-04 13:21:12 -0400

    recieve and log test cookie with cors

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
-const app = express();
-const port = 5000;
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const port = 3001;
 
-app.get('/test', (req, res) => {
+const app = express();
+
+const corsOptions = {
+	origin: true,
+	'Access-Control-Allow-Credentials': true,
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.post('/test', (req, res) => {
+	res.header('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Allow-Credentials', true);
+	if (req.cookies) {
+		console.log('cookie detected');
+	}
+	console.log('cookie: ' + JSON.stringify(req.cookies));
 	res.cookie('test', 'test cookie');
-	res.send('Hello World!');
+	res.json({ cookie: 'check check' });
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello world');
 });
 
 app.listen(port, () => {
